bc4293b733849b6d6b86f527dc77c8a29ee3ad95
sumon <sumonfakir1997@gmail.com>
2025-10-07 15:05:44 +0600

    project setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  staticMode: true,
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwindcss-animate")],
 };
 
 export default nextConfig;
